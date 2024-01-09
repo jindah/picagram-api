deccce9aa751470d13fd3f6595673c00af7afae0
Joonas Timlin <joonas@timlins.se>
2024-01-09 19:02:08 +0000

    Install jwt decode

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
+import styles from '../styles/ShareButton.module.css'
 
 // Share modal for sharing posts to other platforms
 const ShareModal = ({ isOpen, onClose, shareUrl, title }) => {
@@ -8,20 +9,29 @@ const ShareModal = ({ isOpen, onClose, shareUrl, title }) => {
     <Modal show={isOpen} onHide={onClose}>
       <Modal.Body>
         <div className="text-center">
-                <FacebookShareButton url={shareUrl} quote={title}>
-                <Button variant="dark">
-                <i className="fa-brands fa-facebook-f"></i></Button>
-                </FacebookShareButton>{' '}
-                <TwitterShareButton url={shareUrl} title={title}>
-                <Button variant="dark">
-                <i className="fa-brands fa-x-twitter"></i></Button>
-                </TwitterShareButton>{' '}
-                <WhatsappShareButton url={shareUrl} title={title}>
-                <Button variant="dark">
-                <i className="fa-brands fa-whatsapp"></i></Button>
-                </WhatsappShareButton>{' '}
-                <Button variant="dark" onClick={onClose}>
-                <i className="fa-solid fa-xmark"></i></Button>
+          <div>
+            <FacebookShareButton url={shareUrl} quote={title}>
+              <div className={styles.share-button}>
+                <i className="fa-brands fa-facebook-f"></i>
+              </div>
+            </FacebookShareButton>
+
+            <TwitterShareButton url={shareUrl} title={title}>
+              <div className={styles.share-button}>
+                <i className="fa-brands fa-x-twitter"></i>
+              </div>
+            </TwitterShareButton>
+
+            <WhatsappShareButton url={shareUrl} title={title}>
+              <div className={styles.share-button}>
+                <i className="fa-brands fa-whatsapp"></i>
+              </div>
+            </WhatsappShareButton>
+
+            <Button variant="dark" onClick={onClose}>
+              <i className="fa-solid fa-xmark"></i>
+            </Button>
+          </div>
         </div>
       </Modal.Body>
     </Modal>
